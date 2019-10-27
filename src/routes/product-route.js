9255eb978f814ea186a4124104070eb883bedffe
Spinkers <lucasmouraolopes@gmail.com>
2019-10-27 18:08:38 -0300

    Refatorando código, delegando controllers

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,21 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/product-controller');
 
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
- });
+router.post('/', controller.post);
  
-router.put('/:id', (req, res, next) => {
-    let id = req.params.id;
-    res.status(200).send({
-       id: id,
-       item: req.body
-    });
- });
+router.put('/:id', controller.put);
  
-router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
- });
+router.delete('/', controller.delete);
 
 module.exports = router;
