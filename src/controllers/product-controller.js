c715883f547e6af7f7d5306eb644dd86fd3f3a43
Spinkers <lucasmouraolopes@gmail.com>
2019-10-27 19:54:55 -0300

    Criando método POST

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,17 +1,25 @@
 'use strict';
 
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
 exports.post = (req, res, next) => {
-    res.status(201).send(req.body);
+    var product = new Product(req.body);
+    product.save().then(x => {
+        res.status(201).send({ message: 'Produto cadastrado com sucesso!' });
+    }).catch(e => {
+        res.status(400).send({ message: 'Falha ao cadastrar o produto!', data: e });
+    });
 }
 
 exports.put = (req, res, next) => {
     let id = req.params.id;
     res.status(200).send({
-       id: id,
-       item: req.body
+        id: id,
+        item: req.body
     });
- };
+};
 
 exports.delete = (req, res, next) => {
     res.status(200).send(req.body);
- };
+};
