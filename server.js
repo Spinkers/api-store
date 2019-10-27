6a46397d694a4ed63fd646a4ee5e9b8a37f409e1
Spinkers <lucasmouraolopes@gmail.com>
2019-10-27 15:53:13 -0300

    Normalizando a porta

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const debug = require('debug') ('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -22,3 +22,17 @@ app.use('/', route);
 
 server.listen(port);
 console.log("API rodando na porta: " + port);
+
+function normalizePort(val){
+    const port = parseInt(val, 10);
+
+    if(isNaN(port)){
+        return val;
+    }
+
+    if(port >= 0){
+        return port;
+    }
+
+    return false;
+}
