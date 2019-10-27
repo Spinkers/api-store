0ad7b7f56fe36a525e0e247d2db077b47694e7a9
Spinkers <lucasmouraolopes@gmail.com>
2019-10-27 16:19:55 -0300

    Adicionando função onListening

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const http = require('http');
-const debug = require('debug') ('nodestr:server');
+const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
@@ -21,18 +21,52 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
 console.log("API rodando na porta: " + port);
 
-function normalizePort(val){
+function normalizePort(val) {
     const port = parseInt(val, 10);
 
-    if(isNaN(port)){
+    if (isNaN(port)) {
         return val;
     }
 
-    if(port >= 0){
+    if (port >= 0) {
         return port;
     }
 
     return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+        debug('Listening on ' + bind);
 }
