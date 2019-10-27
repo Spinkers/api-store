e33c4878c6644b2139cd4c7e5fe5f669cf22b494
Spinkers <lucasmouraolopes@gmail.com>
2019-10-27 15:12:21 -0300

    Criando servidor

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const http = require('http');
+const debug = require('debug') ('nodestr:server');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node Store API",
+        version: "0.0.1"
+    });
+});
+
+app.use('/', route);
+
+server.listen(port);
+console.log("API rodando na porta: " + port);
