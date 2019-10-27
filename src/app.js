0c5524cfa808c69a331936cef44ebf53b29138a0
Spinkers <lucasmouraolopes@gmail.com>
2019-10-27 17:38:47 -0300

    Criando rotas básicas

--- src/app.js
+++ src/app.js
@@ -7,12 +7,12 @@ const app = express();
 const router = express.Router();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
    res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.1"
+      title: "Node Store API",
+      version: "0.0.1"
    });
 });
 
@@ -20,7 +20,21 @@ const create = router.post('/', (req, res, next) => {
    res.status(201).send(req.body);
 });
 
+const put = router.put('/:id', (req, res, next) => {
+   let id = req.params.id;
+   res.status(200).send({
+      id: id,
+      item: req.body
+   });
+});
+
+const del = router.delete('/', (req, res, next) => {
+   res.status(200).send(req.body);
+});
+
 app.use('/', route);
 app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
