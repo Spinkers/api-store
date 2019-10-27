e8a0aa6aa9f0d99c4663c437bc7450c51c038794
Spinkers <lucasmouraolopes@gmail.com>
2019-10-27 17:28:07 -0300

    Configurando body-parser

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 const route = router.get('/', (req, res, next) => {
    res.status(200).send({
     title: "Node Store API",
     version: "0.0.1"
-   }) ;
+   });
+});
+
+const create = router.post('/', (req, res, next) => {
+   res.status(201).send(req.body);
 });
 
 app.use('/', route);
+app.use('/products', create);
 
 module.exports = app;
