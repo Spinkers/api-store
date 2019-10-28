ad45fb8791577edf1acf9554acf455df4e2b55d9
Spinkers <lucasmouraolopes@gmail.com>
2019-10-28 09:10:04 -0300

    Criando rota de listar produtos

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -4,6 +4,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/product-controller');
 
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+
 router.post('/', controller.post);
  
 router.put('/:id', controller.put);
