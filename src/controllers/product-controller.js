efaadfbadaceced078e52f94496e4ba5b0c811b9
Spinkers <lucasmouraolopes@gmail.com>
2019-10-29 11:22:30 -0300

    Refatorando product-controller

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
 
