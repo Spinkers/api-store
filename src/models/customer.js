37f07f5b7a7a0654aa86129b94941d6d008efe80
Spinkers <lucasmouraolopes@gmail.com>
2019-10-29 11:02:03 -0300

    Criando model Customer

---
+++ src/models/customer.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Customer', schema);
