9bce32fae81a206450649df3f876d813be07428e
Spinkers <lucasmouraolopes@gmail.com>
2019-10-29 11:23:16 -0300

    Criando customer-controller

---
+++ src/controllers/customer-controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const ValidationContract = require('../validators/fluent-validator');
+const repository = require('../repositories/customer-repository');
+
+exports.post = async (req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.name, 3, 'O nome deve conter pelomenos 3 caracteres!');
+    contract.isEmail(req.body.email, 'E-mail inválido!');
+    contract.hasMinLen(req.body.password, 6, 'A senha deve conter pelomenos 6 caracteres!');
+
+    //Se os dados forem inválidos
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
+    try {
+        await repository.create(req.body);
+        res.status(201).send({ message: 'Cliente cadastrado com sucesso!' });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição.'
+        });
+    }
+};
