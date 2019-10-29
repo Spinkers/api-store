03bfc67c77f9ad857b2eae5122c14054d849245b
Spinkers <lucasmouraolopes@gmail.com>
2019-10-29 11:21:43 -0300

    Adicionando models e rotas do customer e order

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ const Order = require('./models/order');
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+const customerRoute = require('./routes/customer-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,5 +38,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
+app.use('/customers', customerRoute);
 
 module.exports = app;
