4b951af38855238337ba86d20a0e8e53f421f32f
Spinkers <lucasmouraolopes@gmail.com>
2019-10-29 11:01:44 -0300

    Importando models Customer e Order

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ mongoose.connect(db.mongoURI,
 
 //Carrega os Models
 const Product = require('./models/product');
+const Customer = require('./models/customer');
+const Order = require('./models/order');
 
 //Carrega as Rotas
 const indexRoute = require('./routes/index-route');
