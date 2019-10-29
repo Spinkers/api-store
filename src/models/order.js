9d853f5e128ecd6247475a54342b7aa37803f20c
Spinkers <lucasmouraolopes@gmail.com>
2019-10-29 11:02:14 -0300

    Criando model Order

---
+++ src/models/order.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer'
+    },
+    number: {
+        type: String,
+        required: true
+    },
+    createDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['created', 'done'],
+        default: 'created'
+    },
+    items: [{
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    }]
+});
+
+module.exports = mongoose.model('Order', schema);
